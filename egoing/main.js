98227ef87f03e31063401be25d7ef3d61d0a721a
dhks3602 <dhks3602@naver.com>
2022-12-21 19:06:21 +0900

    몰아서커밋

---
+++ egoing/main.js
@@ -0,0 +1,13 @@
+'use strict'
+console.log("hello world")
+let a;
+a=6;
+
+//variable
+let name = 'ellie';
+console.log(name);
+
+const bigInt = 12313213213213213210n;
+console.log(bigInt);
+const char = 'c';
+console.log(char);
